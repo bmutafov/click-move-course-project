d1f0988ed7dd6035679c0bf3ba0fd18444fcb7b9
Boris Mutafov <mutafow@gmail.com>
2017-05-13 01:27:14 +0300

    Catch object is now an animated coin. Btw lag is not fixed.

---
+++ Assets/Scripts/Static/CoinRotate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CoinRotate : MonoBehaviour {
+    public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * speed, 0f));
+	}
+}
